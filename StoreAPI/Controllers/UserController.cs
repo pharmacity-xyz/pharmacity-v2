affd15f565a9ddf9c1d27da9bfa9ee739010327e
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-04 08:25:55 +0700

    Update UserController

--- StoreAPI/Controllers/UserController.cs
+++ StoreAPI/Controllers/UserController.cs
@@ -10,38 +10,40 @@ namespace StoreAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MemberController : ControllerBase
+    public class UserController : ControllerBase
     {
 
-        private readonly IMemberRepository memberRepository;
+        private readonly IUserRepository userRepository;
 
-        public MemberController(IMemberRepository memberRepository)
+        public UserController(IUserRepository userRepository)
         {
-            this.memberRepository = memberRepository;
+            this.userRepository = userRepository;
         }
 
         [HttpPost("login")]
-        public IActionResult Login(MemberDTO memberDTO)
+        public IActionResult Login(UserDTO userDTO)
         {
             try
             {
-                if (memberDTO.Email.Equals("") && memberDTO.Password.Equals("")) {                 
+                if (userDTO.Email!.Equals("") && userDTO.Password!.Equals(""))
+                {
                     throw new Exception("Email and Password cannot be empty");
                 }
                 else
                 {
-                    if (memberDTO.Email.Equals("")) throw new Exception("Email cannot be empty");
-                    if (memberDTO.Password.Equals("")) throw new Exception("Password cannot be empty");
+                    if (userDTO.Email.Equals("")) throw new Exception("Email cannot be empty");
+                    if (userDTO.Password!.Equals("")) throw new Exception("Password cannot be empty");
                 }
-                
 
-                MemberDTO member = memberRepository.Login(memberDTO.Email, memberDTO.Password);
+
+                UserDTO member = userRepository.Login(userDTO.Email, userDTO.Password);
 
                 LoggedUser.Instance.User = member;
 
                 return Ok(LoggedUser.Instance.User);
 
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
                 return BadRequest(e.Message);
             }
@@ -52,15 +54,15 @@ namespace StoreAPI.Controllers
         {
             try
             {
-                MemberDTO member = memberRepository.Login(email, password);
+                UserDTO user = userRepository.Login(email, password);
 
                 if (!confirmNewPassword.Equals(newPassword)) throw new Exception("Confirm password does not match new password");
 
-                member.Password = newPassword;
+                user.Password = newPassword;
 
-                memberRepository.Update(member);
+                userRepository.Update(user);
 
-                LoggedUser.Instance.User = member;
+                LoggedUser.Instance.User = user;
 
                 return Ok(LoggedUser.Instance.User);
 
@@ -101,7 +103,7 @@ namespace StoreAPI.Controllers
         }
 
         [HttpPost("register")]
-        public IActionResult register(MemberDTO memberDTO)
+        public IActionResult register(UserDTO userDTO)
         {
             try
             {
@@ -115,8 +117,8 @@ namespace StoreAPI.Controllers
                 //    if (memberDTO.Password.Equals("")) throw new Exception("Password cannot be empty");
                 //}
 
-                memberDTO.Role = Role.USER.ToString();
-                memberRepository.Add(memberDTO);
+                userDTO.Role = Role.USER.ToString();
+                userRepository.Add(userDTO);
 
                 return Ok(LoggedUser.Instance.User);
 
@@ -136,20 +138,20 @@ namespace StoreAPI.Controllers
         {
             try
             {
-                MemberDTO member = LoggedUser.Instance.User;
+                UserDTO user = LoggedUser.Instance.User;
 
-                if (member == null)
+                if (user == null)
                 {
                     throw new Exception("Can't do this action");
                 }
 
-                member.City = newCity;
-                member.Country = newCountry;
-                member.CompanyName = newCompany;
+                user.City = newCity;
+                user.Country = newCountry;
+                user.CompanyName = newCompany;
 
-                memberRepository.Update(member);
+                userRepository.Update(user);
 
-                LoggedUser.Instance.User = member;
+                LoggedUser.Instance.User = user;
 
                 return Ok(LoggedUser.Instance.User);
 
@@ -165,8 +167,8 @@ namespace StoreAPI.Controllers
         {
             try
             {
-                List<MemberDTO> memberDTOs = memberRepository.GetAll();
-                return Ok(memberDTOs);
+                List<UserDTO> userDTOs = userRepository.GetAll();
+                return Ok(userDTOs);
             }
             catch (Exception e)
             {
