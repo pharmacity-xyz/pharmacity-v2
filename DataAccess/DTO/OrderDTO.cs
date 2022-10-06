3d0bde7e4eaaa5f429b2e0a4970be46f9bbdcdf4
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 20:13:06 +0700

    Update DTO

--- DataAccess/DTO/OrderDTO.cs
+++ DataAccess/DTO/OrderDTO.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess.DTO
+﻿namespace DataAccess.DTO
 {
     public class OrderDTO
     {
         public Guid? OrderId { get; set; }
-        public DateTime? OrderedDate { get; set; }
-        public DateTime? ShipDate { get; set; }
+        public float Amount { get; set; }
+        public string ShipAddress { get; set; } = default!;
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippedDate { get; set; }
 
         public Guid UserId { get; set; }
     }
