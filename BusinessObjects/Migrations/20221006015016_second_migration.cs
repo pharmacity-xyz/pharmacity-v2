d1e0605077ecfa285b3c44ca8a30fc2ef9af8b16
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 08:50:36 +0700

    Update Migrations

---
+++ BusinessObjects/Migrations/20221006015016_second_migration.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    public partial class second_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImage_Products_ProductId",
+                table: "ProductImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductImage",
+                table: "ProductImage");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImage");
+
+            migrationBuilder.RenameTable(
+                name: "ProductImage",
+                newName: "ProductImages");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductImages",
+                table: "ProductImages",
+                column: "ProductImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_Products_ProductImageId",
+                table: "ProductImages",
+                column: "ProductImageId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_Products_ProductImageId",
+                table: "ProductImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductImages",
+                table: "ProductImages");
+
+            migrationBuilder.RenameTable(
+                name: "ProductImages",
+                newName: "ProductImage");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductImage",
+                table: "ProductImage",
+                column: "ProductImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImage",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImage_Products_ProductId",
+                table: "ProductImage",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
