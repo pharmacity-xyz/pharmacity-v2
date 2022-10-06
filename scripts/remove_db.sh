c36d52d99ad8f4386eaee6bdd5152ecb1d0e4a76
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 15:24:41 +0700

    Update remove_db.sh

---
+++ scripts/remove_db.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -x
+set -eo pipefail
+
+container_id=$(docker ps -aqf status=running)
+
+echo "Stop docker ${container_id}"
+docker stop ${container_id}
+
+echo "Kill docker ${container_id}"
+docker container kill ${container_id}
+
+echo "Remove Migrations folder"
+rm -rf BusinessObjects/Migrations/
