c18b7e0235f8b58884ccdb77db5758db27ce33f9
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 20:31:55 +0700

    Update OrderDetailDTO

--- DataAccess/DTO/OrderDetailDTO.cs
+++ DataAccess/DTO/OrderDetailDTO.cs
@@ -2,13 +2,12 @@ namespace DataAccess.DTO
 {
     public class OrderDetailDTO
     {
-        public int? OrderId { get; set; }
-        public int? ProductId { get; set; }
-        public string? ProductName { get; set; }
-        public decimal? UnitPrice { get; set; }
+        public Guid? OrderDetailId { get; set; }
+        public float Price { get; set; }
         public int? Quantity { get; set; }
-        public double Discount { get; set; }
-        public double TotalPrice { get; set; }
-        public int CategoryId { get; set; }
+
+        public int? OrderId { get; set; }
+
+        public int? ProductId { get; set; }
     }
 }
