da84f7c80a419a89002dd589823bad09369eb72d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 13:49:02 +0700

    Update Model

--- BusinessObjects/Data/AppDbContext.cs
+++ BusinessObjects/Data/AppDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using BusinessObjects.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessObjects.Data
 {
