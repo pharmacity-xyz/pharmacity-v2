b64c60e9b24e73c2953a9de4ab1619c5181b8163
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-06 09:03:23 +0700

    Implement ProductImageRepositry

---
+++ Repositories/Implements/ProductImageRepositry.cs
@@ -0,0 +1,30 @@
+using BusinessObjects.Model;
+using DataAccess;
+using DataAccess.DTO;
+using DataAccess.Util;
+
+namespace Repositories.Implements
+{
+    public class ProductImageRepository : IProductImageRepository
+    {
+        public void AddNewProductImage(ProductImageDTO p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProductImage(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductImageDTO GetProductImage(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProductImage(ProductImageDTO p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
