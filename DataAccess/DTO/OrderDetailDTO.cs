9a2a621cc1b28da4fadd371a7645644b64ac9e86
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-07 08:16:20 +0700

    Update Add function

--- DataAccess/DTO/OrderDetailDTO.cs
+++ DataAccess/DTO/OrderDetailDTO.cs
@@ -4,10 +4,10 @@ namespace DataAccess.DTO
     {
         public Guid? OrderDetailId { get; set; }
         public float Price { get; set; }
-        public int? Quantity { get; set; }
+        public int Quantity { get; set; }
 
-        public Guid? OrderId { get; set; }
+        public Guid OrderId { get; set; }
 
-        public Guid? ProductId { get; set; }
+        public Guid ProductId { get; set; }
     }
 }
--- Repositories/Implements/OrderDetailRepository.cs
+++ Repositories/Implements/OrderDetailRepository.cs
@@ -7,9 +7,18 @@ namespace Repositories.Implements
 {
     public class OrderDetailRepository : IOrderDetailRepository
     {
-        public void Add(OrderDetailDTO orderDetail)
+        public void Add(OrderDetailDTO orderDetailDTO)
         {
-            throw new NotImplementedException();
+            OrderDetail newOrderDetail = new OrderDetail
+            {
+
+                OrderDetailId = Guid.NewGuid(),
+                Price = orderDetailDTO.Price,
+                Quantity = orderDetailDTO.Quantity,
+                OrderId = orderDetailDTO.OrderId,
+                ProductId = orderDetailDTO.ProductId,
+            };
+            OrderDetailDAO.Instance.Add(newOrderDetail);
         }
 
         public void Delete(Guid id)
