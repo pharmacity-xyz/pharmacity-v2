7407b8923013d2e253eea69922816c472c5641ec
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-07 08:26:24 +0700

    Remove db

--- scripts/remove_db.sh
+++ scripts/remove_db.sh
@@ -7,8 +7,8 @@ container_id=$(docker ps -aqf status=running)
 echo "Stop docker ${container_id}"
 docker stop ${container_id}
 
-echo "Kill docker ${container_id}"
-docker container kill ${container_id}
+echo "Remove docker ${container_id}"
+docker container rm ${container_id}
 
 echo "Remove Migrations folder"
 rm -rf BusinessObjects/Migrations/
