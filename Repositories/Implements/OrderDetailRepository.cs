b941db9cedb00ca29120ab1150f026a646e9ca12
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-07 08:06:09 +0700

    Create OrderDetailRepository

---
+++ Repositories/Implements/OrderDetailRepository.cs
@@ -0,0 +1,30 @@
+using DataAccess;
+using DataAccess.DTO;
+using DataAccess.Util;
+using BusinessObjects.Model;
+
+namespace Repositories.Implements
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        public void Add(OrderDetailDTO orderDetail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderDetailDTO GetOrderDetailByOrderID(Guid orderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(OrderDetailDTO orderDetail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
