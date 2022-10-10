eab37c926c94487e37949301cf09ea7c247fe169
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-10 11:30:00 +0700

    Create a data

---
+++ StoreAPI/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObjects.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace StoreAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("MyStoreDB"));
+        }
+
+        public virtual DbSet<User>? Users { get; set; }
+        public virtual DbSet<Category>? Categories { get; set; }
+        public virtual DbSet<Product>? Products { get; set; }
+        public virtual DbSet<ProductImage>? ProductImages { get; set; }
+        public virtual DbSet<Order>? Orders { get; set; }
+        public virtual DbSet<OrderDetail>? OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.ProductImage)
+                .WithOne(i => i.Product)
+                .HasForeignKey<ProductImage>(pi => pi.ProductImageId);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ StoreAPI/Data/UserConfiguration.cs
@@ -0,0 +1,15 @@
+using BusinessObjects.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StoreAPI.Data
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasIndex(user => user.Email).IsUnique();
+        }
+
+    }
+}
