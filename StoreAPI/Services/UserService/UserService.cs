fc9857753e169be00d563854404006bdfec7c77a
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-11 14:28:50 +0700

    Update UserService

--- StoreAPI/Services/UserService/UserService.cs
+++ StoreAPI/Services/UserService/UserService.cs
@@ -15,77 +15,16 @@ namespace StoreAPI.Services
         private readonly AppDbContext _context;
         private readonly IAuthService _authService;
 
-        public UserService(
-            AppDbContext context,
-            IAuthService authService)
+        public UserService(AppDbContext context, IAuthService authService)
         {
             _context = context;
             _authService = authService;
         }
 
-        // public Guid GetUserId() => Guid.Parse(
-        //     _httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-        // public string GetUserEmail() => _httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.Email);
-
-
-        public List<UserDTO> GetAll()
-        {
-            // return UserDAO.Instance.FetchAllUsers().Select(m => UserMapper.mapToDTO(m)).ToList()!;
-            throw new NotImplementedException();
-        }
-
-        public UserDTO GetLoggedAccount()
+        public Task<ServiceResponse<List<User>>> GetAll()
         {
             throw new NotImplementedException();
         }
-
-        // private string CreateToken(User user)
-        // {
-        //     List<Claim> claims = new List<Claim> {
-        //         new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
-        //         new Claim(ClaimTypes.Email, user.Email),
-        //         new Claim(ClaimTypes.Role, user.Role!),
-        //     };
-
-        //     var key = new SymmetricSecurityKey(
-        //         System.Text.Encoding.UTF8.GetBytes(
-        //             _configuration.GetSection("AppSettings:Token").Value
-        //         )
-        //     );
-
-        //     var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-        //     var token = new JwtSecurityToken(
-        //         claims: claims,
-        //         expires: DateTime.Now.AddDays(1),
-        //         signingCredentials: cred
-        //     );
-
-        //     var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-        //     return jwt;
-        // }
-
-        // private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        // {
-        //     using (var hmac = new HMACSHA512())
-        //     {
-        //         passwordSalt = hmac.Key;
-        //         passwordHash = hmac
-        //             .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        //     }
-        // }
-
-        // private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-        // {
-        //     using (var hmac = new HMACSHA512(passwordSalt))
-        //     {
-        //         var computedHash =
-        //             hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        //         return computedHash.SequenceEqual(passwordHash);
-        //     }
-        // }
 
         public async Task<ServiceResponse<User>> GetUser()
         {
