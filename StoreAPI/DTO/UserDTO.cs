68c362d203b3a60e456ac8cc42451b4281c95d4a
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-11 08:36:17 +0700

    Update DTO

---
+++ StoreAPI/DTO/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace StoreAPI.DTO
+{
+    public class UserDTO
+    {
+        public Guid? UserId { get; set; }
+
+        public string Email { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+
+        public string FirstName { get; set; } = default!;
+
+        public string LastName { get; set; } = default!;
+
+        public string City { get; set; } = default!;
+
+        public string Country { get; set; } = default!;
+
+        public string CompanyName { get; set; } = default!;
+
+        public string? Role { get; set; }
+    }
+}
