f8051fa9e4b6fdcfb9c3dbc3678d0bfed71e29c6
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-11 14:05:57 +0700

    Create AuthService

---
+++ StoreAPI/Services/AuthService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using StoreAPI.Models;
+using StoreAPI.Utils;
+
+namespace StoreAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<Guid>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string email, string password);
+        Task<ServiceResponse<bool>> ChangePassword(Guid userId, string newPassword);
+    }
+}
