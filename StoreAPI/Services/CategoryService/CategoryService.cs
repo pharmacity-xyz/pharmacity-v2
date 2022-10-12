0c9d65b5cd63e6f47f763248a600a3b821155f13
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-12 07:29:52 +0700

    Remove comments CategoryService

--- StoreAPI/Services/CategoryService/CategoryService.cs
+++ StoreAPI/Services/CategoryService/CategoryService.cs
@@ -1,14 +1,42 @@
 ﻿using StoreAPI.DTO;
 using StoreAPI.Models;
 using StoreAPI.Utils;
-// using DataAccess;
-// using DataAccess.DTO;
-// using DataAccess.Util;
 
 namespace StoreAPI.Services
 {
     public class CategoryService : ICategoryService
     {
+        private readonly DataContext _context;
+
+        public CategoryService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<Category>>> GetAdminCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<Category>>> AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<Category>>> UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<Category>>> DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
         public void Add(Category category)
         {
             // Category newCategory = new Category
@@ -50,29 +78,6 @@ namespace StoreAPI.Services
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<List<Category>>> GetCategories()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<ServiceResponse<List<Category>>> GetAdminCategories()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<List<Category>>> AddCategory(Category category)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<List<Category>>> UpdateCategory(Category category)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<List<Category>>> DeleteCategory(int id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
