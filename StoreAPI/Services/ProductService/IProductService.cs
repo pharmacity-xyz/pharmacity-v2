6b25c7de8f9a023ee553c229eb59a3df87648701
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-12 13:29:29 +0700

    Update ProductSerarchResult

--- StoreAPI/Services/ProductService/IProductService.cs
+++ StoreAPI/Services/ProductService/IProductService.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using StoreAPI.Utils;
 using StoreAPI.DTO;
 using StoreAPI.Models;
 
@@ -12,5 +12,16 @@ namespace StoreAPI.Services
         ProductDTO GetProductById(Guid id);
         void UpdateProduct(ProductDTO p);
         void DeleteProduct(Guid id);
+
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+        Task<ServiceResponse<ProductSearchResult>> SearchProducts(string searchText, int page);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
+        Task<ServiceResponse<List<Product>>> GetFeaturedProducts();
+        Task<ServiceResponse<List<Product>>> GetAdminProducts();
+        Task<ServiceResponse<Product>> CreateProduct(Product product);
+        Task<ServiceResponse<Product>> UpdateProduct(Product product);
+        Task<ServiceResponse<bool>> DeleteProduct(int productId);
     }
 }
---
+++ StoreAPI/Utils/ProductSearchResult.cs
@@ -0,0 +1,11 @@
+using StoreAPI.Models;
+
+namespace BlazorEcommerce.Shared
+{
+    public class ProductSearchResult
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
