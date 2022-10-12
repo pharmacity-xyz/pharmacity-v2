2eb8a482f58e9317d669844dd31d684196cab358
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-12 13:36:36 +0700

    Update ProductDTO

--- StoreAPI/DTO/Product/ProductDTO.cs
+++ StoreAPI/DTO/Product/ProductDTO.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StoreAPI.DTO
 {
     public class ProductDTO
     {
         public Guid? ProductId { get; set; }
-        public string ProductName { get; set; } = default!;
-        public string ProductDescription { get; set; } = default!;
+
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+
+        [Required]
+        public string ProductDescription { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+        [Required]
         public int Stock { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
 
         public Guid CategoryId { get; set; }
--- StoreAPI/Models/Product.cs
+++ StoreAPI/Models/Product.cs
@@ -6,16 +6,13 @@ namespace StoreAPI.Models
     {
         [Key]
         public Guid ProductId { get; set; }
-        [Required]
         public string ProductName { get; set; } = string.Empty;
         public string ProductDescription { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public List<ProductImage> Images { get; set; } = new List<ProductImage>();
-        [Required]
         public int Stock { get; set; }
-
-        [Required]
         public decimal Price { get; set; }
+        public bool Featured { get; set; } = false;
 
         public virtual Category? Category { get; set; }
         public Guid CategoryId { get; set; }
