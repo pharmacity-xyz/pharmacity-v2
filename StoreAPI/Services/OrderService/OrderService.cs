9bc1cc5f62043ba44d6102b3b0de55ab4d1e1645
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-14 11:29:42 +0700

    OrderService function

--- StoreAPI/Services/OrderService/OrderService.cs
+++ StoreAPI/Services/OrderService/OrderService.cs
@@ -58,14 +58,64 @@ namespace StoreAPI.Services
             return response;
         }
 
-        public Task<ServiceResponse<List<OrderOverviewResponse>>> GetOrders()
+        public async Task<ServiceResponse<List<OrderOverviewResponse>>> GetOrders()
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<List<OrderOverviewResponse>>();
+            var orders = await _context.Orders!
+                .Include(o => o.OrderItems!)
+                .ThenInclude(oi => oi.Product)
+                .Where(o => o.UserId == _authService.GetUserId())
+                .OrderByDescending(o => o.OrderDate)
+                .ToListAsync();
+
+            var orderResponse = new List<OrderOverviewResponse>();
+            orders.ForEach(o => orderResponse.Add(new OrderOverviewResponse
+            {
+                Id = o.OrderId,
+                OrderDate = o.OrderDate,
+                TotalPrice = o.TotalPrice,
+                Product = o.OrderItems!.Count > 1 ?
+                    $"{o.OrderItems.First().Product!.ProductName} and" +
+                    $" {o.OrderItems.Count - 1} more..." :
+                    o.OrderItems.First().Product!.ProductName,
+                ProductImageUrl = o.OrderItems.First().Product!.ImageUrl
+            }));
+
+            response.Data = orderResponse;
+
+            return response;
         }
 
-        public Task<ServiceResponse<bool>> PlaceOrder(Guid userId)
+        public async Task<ServiceResponse<bool>> PlaceOrder(Guid userId)
         {
-            throw new NotImplementedException();
+            var products = (await _cartService.GetDbCartProducts(userId)).Data;
+            decimal totalPrice = 0;
+            products!.ForEach(product => totalPrice += product.Price * product.Quantity);
+
+            var orderItems = new List<OrderItem>();
+            products.ForEach(product => orderItems.Add(new OrderItem
+            {
+                ProductId = product.ProductId,
+                Quantity = product.Quantity,
+                TotalPrice = product.Price * product.Quantity
+            }));
+
+            var order = new Order
+            {
+                UserId = userId,
+                OrderDate = DateTime.Now,
+                TotalPrice = totalPrice,
+                OrderItems = orderItems
+            };
+
+            _context.Orders!.Add(order);
+
+            _context.CartItems!.RemoveRange(_context.CartItems
+                .Where(ci => ci.UserId == userId));
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool> { Data = true };
         }
     }
 }
