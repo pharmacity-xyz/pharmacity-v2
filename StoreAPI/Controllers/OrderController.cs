bac4ebed1fa3bd2a6ac83c7add1944f283924086
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-14 11:00:31 +0700

    Update GetOrders function

--- StoreAPI/Controllers/OrderController.cs
+++ StoreAPI/Controllers/OrderController.cs
@@ -11,116 +11,89 @@ namespace StoreAPI.Controllers
     public class OrderController : ControllerBase
     {
         private readonly IOrderService _orderService;
-        // private readonly IOrderDetailRepository orderDetailRepository;
-        // private readonly IProductRepository productRepository;
 
-        public OrderController(
-            IOrderService orderService
-        )
+        public OrderController(IOrderService orderService)
         {
             _orderService = orderService;
-            // this.orderDetailRepository = orderDetailRepository;
-            // this.productRepository = productRepository;
         }
 
-        [HttpPost("add")]
-        public IActionResult Add(OrderDTO newOrder)
+        // [HttpPost("add")]
+        // public IActionResult Add(OrderDTO newOrder)
+        // {
+        //     try
+        //     {
+        //         newOrder.ShippedDate = DateTime.UtcNow;
+        //         Guid orderId = _orderService.Add(newOrder);
+        //         // orderDetailRepository.Add(newOrder.OrderDetail!, orderId);
+
+        //         return Ok("Successfully added");
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return BadRequest(e.Message);
+        //     }
+        // }
+
+        [HttpGet]
+        public IActionResult GetOrders()
         {
-            try
+            IEnumerable<OrderDTO> orderList = _orderService.GetAllOrders();
+            foreach (OrderDTO orderDTO in orderList)
             {
-                newOrder.ShippedDate = DateTime.UtcNow;
-                Guid orderId = _orderService.Add(newOrder);
-                // orderDetailRepository.Add(newOrder.OrderDetail!, orderId);
-
-                return Ok("Successfully added");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpGet("get_all")]
-        public IActionResult GetAll()
-        {
-            try
-            {
-                IEnumerable<OrderDTO> orderList = _orderService.GetAllOrders();
-                foreach (OrderDTO orderDTO in orderList)
-                {
-                    // orderDTO.OrderDetail = orderDetailRepository.GetOrderDetailByOrderID(orderDTO.OrderId);
-                }
-                return Ok(orderList);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpGet("get_by_id/{id}"), Authorize(Roles = "User")]
-        public IActionResult GetId(Guid id)
-        {
-
-            try
-            {
-                // UserDTO user = LoggedUser.Instance!.User!;
-
-                // if (user == null)
-                // {
-                //     throw new Exception("Please login");
-                // }
-
-                OrderDTO orderDTO = _orderService.GetOrderById(id);
                 // orderDTO.OrderDetail = orderDetailRepository.GetOrderDetailByOrderID(orderDTO.OrderId);
-
-                return Ok(orderDTO);
             }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
+            return Ok(orderList);
         }
 
-        [HttpGet("get_all_by_userid/{userid}")]
-        public IActionResult GetAll(Guid userid)
+        [HttpGet("{id}")]
+        public IActionResult GetId(Guid orderId)
         {
-            try
-            {
-                // UserDTO user = LoggedUser.Instance!.User!;
+            OrderDTO orderDTO = _orderService.GetOrderById(orderId);
+            // orderDTO.OrderDetail = orderDetailRepository.GetOrderDetailByOrderID(orderDTO.OrderId);
 
-                // if (user == null)
-                // {
-                //     throw new Exception("Please login");
-                // }
+            return Ok(orderDTO);
 
-                // IEnumerable<OrderDTO> orderList = orderRepository.GetAllOrdersByUserId(userid);
-                // foreach (OrderDTO orderDTO in orderList)
-                // {
-                //     orderDTO.OrderDetail = orderDetailRepository.GetOrderDetailByOrderID(orderDTO.OrderId);
-                // }
-                // return Ok(orderList);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
         }
 
-        [HttpDelete("delete/{id}")]
-        public IActionResult Delete(Guid id)
-        {
-            try
-            {
-                // orderDetailRepo.Delete(id);
-                _orderService.Delete(id);
-                return Ok("Successfully deleted");
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
+        // [HttpGet("get_all_by_userid/{userid}")]
+        // public IActionResult GetAll(Guid userid)
+        // {
+        //     try
+        //     {
+        //         // UserDTO user = LoggedUser.Instance!.User!;
+
+        //         // if (user == null)
+        //         // {
+        //         //     throw new Exception("Please login");
+        //         // }
+
+        //         // IEnumerable<OrderDTO> orderList = orderRepository.GetAllOrdersByUserId(userid);
+        //         // foreach (OrderDTO orderDTO in orderList)
+        //         // {
+        //         //     orderDTO.OrderDetail = orderDetailRepository.GetOrderDetailByOrderID(orderDTO.OrderId);
+        //         // }
+        //         // return Ok(orderList);
+        //         return Ok();
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return BadRequest(e.Message);
+        //     }
+        // }
+
+        // [HttpDelete("delete/{id}")]
+        // public IActionResult Delete(Guid id)
+        // {
+        //     try
+        //     {
+        //         // orderDetailRepo.Delete(id);
+        //         _orderService.Delete(id);
+        //         return Ok("Successfully deleted");
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return BadRequest(e.Message);
+        //     }
+        // }
     }
 }
